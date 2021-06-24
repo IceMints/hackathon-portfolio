8aed948e6920476f09cc0a41a9449a41566fa0fe
IceMints <IceMints@github.com>
2021-06-24 07:52:38 +0000

    add login and register routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, abort, url_for, request
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
+from werkzeug.security import generate_password_hash, check_password_hash
 # loads the data files
 from app.load_data import load_projects, load_profiles
 from . import db
@@ -54,6 +55,57 @@ def blog():
 def page(path):
     page = pages.get_or_404(path)
     return render_template('page.html', page=page, title="Blog")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
+                error = f"User {username} is already registered."
+        
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a register page
+    return "Register Page not yet implemented", 501
+        
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)).fetchone()
+            
+            if user is None:
+                error = 'Incorrect username.'
+            elif not check_password_hash(user['password'], password):
+                error = 'Incorrect password.'
+
+            if error is None:
+                return "Login Successful", 200
+            else:
+                return error, 418
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
 
 @app.errorhandler(404)
 def page_not_found(e):
