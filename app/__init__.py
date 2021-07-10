fea39b71b85b2d1cc35176e372eeb1410445a437
IceMints <IceMints@github.com>
2021-07-10 04:01:56 +0000

    workflows

--- app/__init__.py
+++ app/__init__.py
@@ -6,24 +6,27 @@ from flask_frozen import Freezer
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+
 # loads the data files
 from app.load_data import load_projects, load_profiles
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
+FLATPAGES_EXTENSION = ".md"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB')
+    table=os.getenv("POSTGRES_DB"),
 )
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -32,9 +35,8 @@ pages = FlatPages(app)
 freezer = Freezer(app)
 
 
-
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -44,7 +46,8 @@ class UserModel(db.Model):
         self.password = password
 
     def __repre__(self):
-        return f'<User {self.username}>'
+        return f"<User {self.username}>"
+
 
 base_url = os.getenv("URL")
 projects_base_url = base_url + "/projects/"
@@ -53,45 +56,59 @@ profiles_base_url = base_url + "/profiles/"
 projects = load_projects()
 profiles = load_profiles()
 
-@app.route('/')
-def index():
-    return render_template('index.html', profiles=profiles, projects=projects, title="Portfolio",
-                           url=base_url)
 
-@app.route('/projects/<name>')
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        profiles=profiles,
+        projects=projects,
+        title="Portfolio",
+        url=base_url,
+    )
+
+
+@app.route("/projects/<name>")
 def get_project(name):
     if name not in projects:
         return abort(404)
-    return render_template('project.html', item=projects[name], title=name, url=projects_base_url + name)
+    return render_template(
+        "project.html", item=projects[name], title=name, url=projects_base_url + name
+    )
 
 
-@app.route('/profiles/<name>')
+@app.route("/profiles/<name>")
 def get_profile(name):
     if name not in profiles:
         return abort(404)
     title = name + "'s Profile"
-    return render_template('profile.html', item=profiles[name], title=title, url=profiles_base_url + name)
+    return render_template(
+        "profile.html", item=profiles[name], title=title, url=profiles_base_url + name
+    )
 
-@app.route('/blog')
+
+@app.route("/blog")
 def blog():
-    return render_template('blog.html', pages=pages, title="Blog")
+    return render_template("blog.html", pages=pages, title="Blog")
 
-@app.route('/<path:path>.html')
+
+@app.route("/<path:path>.html")
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('page.html', page=page, title="Blog")
+    return render_template("page.html", page=page, title="Blog")
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -103,37 +120,41 @@ def register():
         else:
             return error, 418
 
-    return render_template('register.html', title='Register')
-        
-@app.route('/login', methods=['GET', 'POST'])
+    return render_template("register.html", title="Register")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
-        
+
         try:
             if user is None:
-                error = 'Incorrect username.'
+                error = "Incorrect username."
             elif not check_password_hash(user.password, password):
-                error = 'Incorrect password.'
+                error = "Incorrect password."
             if error is None:
                 return "Login Successful", 200
             else:
                 return error, 418
         except:
-            return 'Incorrect password.'
-    return render_template('login.html', title='Login')
+            return "Incorrect password."
+    return render_template("login.html", title="Login")
 
-@app.route('/<username>/home')
+
+@app.route("/<username>/home")
 def home(username):
-    return render_template('userhome.html', title=username)
+    return render_template("userhome.html", title=username)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', title="Page not found"), 404
+    return render_template("404.html", title="Page not found"), 404
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
-    return '', 200
+    return "", 200
