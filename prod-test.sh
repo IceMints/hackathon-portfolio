dea15000800358d59ab054b62370f40b2f971227
IceMints <IceMints@github.com>
2021-07-10 02:29:32 -0700

    removed colors from script

--- prod-test.sh
+++ prod-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Color Codes
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-purple=`tput setaf 5`
-white=`tput setaf 7`
-cyan=`tput setaf 6`
-reset_color=`tput sgr0`
+# red=`tput setaf 1`
+# green=`tput setaf 2`
+# yellow=`tput setaf 3`
+# purple=`tput setaf 5`
+# white=`tput setaf 7`
+# cyan=`tput setaf 6`
+# reset_color=`tput sgr0`
 exitstatus=0
 
 # website data
@@ -16,20 +16,20 @@ PAGES="/ /blog /health /login /register /profiles/Gigi /projects/Portfolio"
 METHODS="GET POST DELETE PUT PATCH"
 
 # Testing endpoints with request methods
-echo ${cyan}Testing endpoints with request methods${reset_color}
+echo Testing endpoints with request methods
 for PAGE in $PAGES; do
 for METHOD in $METHODS; do
 
 GETVAR=$(curl -LI $URL$PAGE -o /dev/null -w '%{http_code}\n' -s -X $METHOD) 
 STATCAT=${GETVAR:0:1}
 if [[ $STATCAT == 2 ]]; then
-echo $PAGE ${white}$METHOD${reset_color} $GETVAR ${green}'SUCCESS'${reset_color}
+echo $PAGE $METHOD $GETVAR'SUCCESS'
 elif [[ $GETVAR == 418 ]]; then
-echo $PAGE ${white}$METHOD${reset_color} $GETVAR ${purple}"I'm a teapot"${reset_color}
+echo $PAGE $METHOD $GETVAR "I'm a teapot"
 elif [[ $GETVAR == 405 ]]; then 
-echo $PAGE ${white}$METHOD${reset_color} $GETVAR ${yellow}'METHOD NOT ALLOWED'${reset_color}
+echo $PAGE $METHOD $GETVAR 'METHOD NOT ALLOWED'
 else
-echo $PAGE ${white}$METHOD${reset_color} $GETVAR ${red}'ERROR'${reset_color}
+echo $PAGE $METHOD $GETVAR 'ERROR'
 exitstatus=1
 fi
 done
@@ -39,43 +39,43 @@ done
 testing_endpoint () {
 GETVAR=$1
 if [[ $GETVAR == $2 ]]; then
-echo $GETVAR ${green}'SUCCESS'${reset_color}
+echo $GETVAR 'SUCCESS'
 else
-echo $GETVAR ${red}'ERROR'${reset_color}
+echo $GETVAR 'ERROR'
 exitstatus=2
 fi
 }
 
 # Testing /register endpoint with post
-echo ${cyan}Testing register endpoint ${reset_color}
+echo Testing register endpoint 
 testing_endpoint "$(curl -sX POST $URL'/register')" "Username is required."
 
-echo ${cyan}Posting to URL with username ${reset_color}
+echo Posting to URL with username 
 testing_endpoint "$(curl -sX POST -d "username=testing" $URL'/register')" "Password is required."
 
 # Set username to test
 USERNAME=test
 
-echo ${cyan}Posting an existing username and password ${reset_color}
+echo Posting an existing username and password 
 testing_endpoint "$(curl -sX POST -d "username=$USERNAME&password=test" $URL'/register')" "User test is already registered."
 
 # Creates a random user to be created
 COUNT=$((1 + $RANDOM % 1000000))
 
-echo ${cyan}Posting with new username and password ${reset_color}
+echo Posting with new username and password 
 testing_endpoint "$(curl -sX POST -d "username=$USERNAME$COUNT&password=test" $URL'/register')" "User $USERNAME$COUNT created successfully"
 
 # Testing /login endpoint with post
-echo ${cyan}Posting to /login endpoint ${reset_color}
+echo Posting to /login endpoint 
 testing_endpoint "$(curl -sX POST $URL'/login')" "Incorrect username."
 
-echo ${cyan}Posting to /login endpoint with unregister user${reset_color}
+echo Posting to /login endpoint with unregister user
 testing_endpoint "$(curl -sX POST -d "username=scooby$COUNT" POST $URL'/login')" "Incorrect username."
 
-echo ${cyan}Posting to /login endpoint with register user and empty password${reset_color}
+echo Posting to /login endpoint with register user and empty password
 testing_endpoint "$(curl -sX POST -d "username=test" POST $URL'/login')" "Incorrect password."
 
-echo ${cyan}Posting to /login endpoint with registered user and correct password${reset_color}
+echo Posting to /login endpoint with registered user and correct password
 testing_endpoint "$(curl -sX POST -d "username=test&password=test" POST $URL'/login')" "Login Successful"
 
 exit $exitstatus
